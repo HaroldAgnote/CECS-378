57015b46bd3002f62104ab5b49f30f453a3ffe58
Harold Agnote <haroldagnote38@hotmail.com>
2018-02-07 16:00:17 -0800

    Updating server.js

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 var http = require("http");
 var connect = require('connect');
+var serveStatic = require('serve-static')
 
 console.log('\n\n--- Node Version: ' + process.version + ' ---');
 
 // Set up Connect routing
 var app = connect()
-    .use(connect.static(__dirname + '/public'))
+    .use(serveStatic(__dirname + '/public'))
     .use(function(req, res) {
         console.log('Could not find handler for: ' + req.url);
         res.end('Could not find handler for: ' + req.url);

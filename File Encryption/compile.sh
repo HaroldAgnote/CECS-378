e1e7882de544b3dec6c8020a2dd01567d309b69f
Harold Agnote <haroldagnote38@hotmail.com>
2018-05-06 21:47:54 -0700

    Modified compile.sh

--- File Encryption/compile.sh
+++ File Encryption/compile.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+echo "Purging files folder"
+rm files/* -rf
+
 echo "Building payload.exe"
 pyinstaller src/payload.py -F
 cp dist/payload files
 
-echo "Removing existing pem files"
-rm files/*pem -rf
-
 echo "Building MyUnlock.exe"
 pyinstaller src/MyUnlock.py -F
 cp dist/MyUnlock files
+
+
+echo "Copying orig_files to files"
+cp ./orig_files/* ./files -rf

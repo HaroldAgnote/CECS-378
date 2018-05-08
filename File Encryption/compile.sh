e57d20eff720e7676a4f4573a5b9875bf41a2700
Harold Agnote <haroldagnote38@hotmail.com>
2018-05-08 03:36:00 +0000

    Modified compile script

--- File Encryption/compile.sh
+++ File Encryption/compile.sh
@@ -11,6 +11,12 @@ echo "Building MyUnlock.exe"
 pyinstaller src/MyUnlock.py -F
 cp dist/MyUnlock files
 
+cp dist/payload* ../TLS_Server/payload
+cp dist/MyUnlock* ../TLS_Server/MyUnlock
+
+chmod 774 ../TLS_Server/payload/*
+chmod 774 ../TLS_Server/MyUnlock/*
+
 
 echo "Copying orig_files to files"
 cp ./orig_files/* ./files -rf
